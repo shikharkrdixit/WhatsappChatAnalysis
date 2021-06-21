413dc3f599fdd2d3c6b5298b869ca53ffd6a7965
Shikhar Dixit <sdixit362@gmail.com>
2021-06-22 02:34:16 +0530

    Commit-1

---
+++ Half Analysis.R
@@ -0,0 +1,62 @@
+setwd("D:/R/WhatsappChatAnalysis")
+library(rwhatsapp)
+library(ggplot2)
+library(lubridate)
+library(ggimage)
+library(tidytext)
+library(stopwords)
+library(tidyverse)
+library(tidymodels)
+
+chat <- rwa_read("./WhatsApp Chat with Srivathsan SRM.txt") %>%  filter(!is.na(author))
+head(chat,3)
+
+chat$Date <- as.Date(chat$time)
+chat$Time <- format(as.POSIXct(chat$time),format="%H:%M:%S")
+names(chat)[names(chat) == "time"] <- "DateAndTime"
+head(chat)
+
+
+
+chat %>% 
+  count(author) %>%
+  ggplot(aes(x = reorder(author, n), y = n, fill=author)) +
+  geom_bar(stat = "identity") + xlab("Author") + ylab("Number Of Messages") +
+  coord_flip() + theme_bw() +
+  ggtitle("Relation between the messages sent by Each Person")
+
+
+
+chat %>%
+  count(Date) %>%
+  ggplot(aes(x = Date, y = n)) + aes(fill=Date) +
+  geom_bar(stat = "identity") + theme_bw() +
+  ylab("Number Of Messages") + xlab("Months") + ggtitle("Chat Timeline")
+
+
+
+
+to_remove <- c(stopwords(kind = 'en'),"media","omitted","ref","3","yes","yess","stu","us","go",
+               "bro","one","well","get","just","idk","fuck")
+chatx <- chat%>% unnest_tokens(input = text,output = word) %>%
+  filter(!word %in% to_remove)
+
+chatx %>% count(author, word, sort = TRUE) %>%
+  group_by(author) %>%
+  top_n(n = 6, n) %>%
+  ggplot(aes(x = reorder_within(word, n, author), y = n, fill = author)) +
+  geom_col(show.legend = FALSE) +
+  ylab("") +
+  xlab("") +
+  coord_flip() +
+  facet_wrap(~author, ncol = 2, scales = "free_y") +
+  scale_x_reordered() +
+  ggtitle("Most often used words")  
+
+library(wordcloud)
+wordcloud(chatx$word,max.words = 50,random.order = F,colors=brewer.pal(8,"Dark2"),scale=c(2,1))
+
+
+
+
+
